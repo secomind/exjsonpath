66174881ff502d344cac5b13c37d285bebf37896
Davide Bettio <davide.bettio@ispirata.com>
2019-06-21 12:21:15 +0200

    Initialize project
    mix new exjsonpath --module ExJsonPath

---
+++ lib/ex_json_path.ex
@@ -0,0 +1,18 @@
+defmodule ExJsonPath do
+  @moduledoc """
+  Documentation for ExJsonPath.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExJsonPath.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_json_path_test.exs
@@ -0,0 +1,8 @@
+defmodule ExJsonPathTest do
+  use ExUnit.Case
+  doctest ExJsonPath
+
+  test "greets the world" do
+    assert ExJsonPath.hello() == :world
+  end
+end
