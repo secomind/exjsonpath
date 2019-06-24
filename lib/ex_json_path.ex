ef010a7cdb158782feceb2647b4357e465e34c6b
Davide Bettio <davide.bettio@ispirata.com>
2019-06-24 12:58:05 +0200

    Start implementing ExJsonPath module
    Implement ExJsonPath basic features.

--- lib/ex_json_path.ex
+++ lib/ex_json_path.ex
@@ -1,18 +1,82 @@
+#
+# This file is part of ExJsonPath.
+#
+# Copyright 2019 Ispirata Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 defmodule ExJsonPath do
-  @moduledoc """
-  Documentation for ExJsonPath.
-  """
+  def eval(map, path) when is_map(map) and is_binary(path) do
+    with {:ok, compiled} <- compile(path) do
+      eval(map, compiled)
+    end
+  end
 
-  @doc """
-  Hello world.
+  def eval(map, compiled_path) when is_map(map) and is_list(compiled_path) do
+    recurse(map, compiled_path)
+  end
 
-  ## Examples
+  def compile(path) when is_binary(path) do
+    with charlist = String.to_charlist(path),
+         {:ok, tokens, _} <- :jsonpath_lexer.string(charlist) do
+      :jsonpath_parser.parse(tokens)
+    end
+  end
 
-      iex> ExJsonPath.hello()
-      :world
+  defp recurse(item, []) do
+    item
+  end
 
-  """
-  def hello do
-    :world
+  defp recurse(enumerable, [{:access, {op, path, value}} | t])
+       when is_list(enumerable) or is_map(enumerable) do
+    Enum.reduce(enumerable, [], fn entry, acc ->
+      item =
+        case entry do
+          {_key, value} -> value
+          item -> item
+        end
+
+      value_at_path = recurse(item, path)
+
+      if compare(op, value_at_path, value) do
+        [recurse(item, t) | acc]
+      else
+        acc
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp recurse(map, [{:access, a} | t]) when is_map(map) do
+    case Map.fetch(map, a) do
+      {:ok, next_item} -> recurse(next_item, t)
+      :error -> {:error, :no_match}
+    end
+  end
+
+  defp recurse(array, [{:access, a} | t]) when is_list(array) and is_integer(a) do
+    next_item = Enum.at(array, a)
+    recurse(next_item, t)
+  end
+
+  defp compare(op, value1, value2) do
+    case op do
+      :== ->
+        value1 == value2
+
+      :!= ->
+        value1 != value2
+    end
   end
 end
--- test/ex_json_path_test.exs
+++ test/ex_json_path_test.exs
@@ -1,8 +1,97 @@
+#
+# This file is part of ExJsonPath.
+#
+# Copyright 2019 Ispirata Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 defmodule ExJsonPathTest do
   use ExUnit.Case
   doctest ExJsonPath
 
-  test "greets the world" do
-    assert ExJsonPath.hello() == :world
+  test "eval a simple path" do
+    map = %{"hello" => %{"world" => "test"}}
+    path = "$.hello.world"
+
+    assert ExJsonPath.eval(map, path) == "test"
+  end
+
+  test "eval a simple path with array" do
+    map = %{"data" => %{"values" => [0, -1, 1, -2]}}
+    path = "$.data.values[1]"
+
+    assert ExJsonPath.eval(map, path) == -1
+  end
+
+  test "eval a simple path with array and a further object" do
+    map = %{"data" => %{"values" => [%{"value" => 0.1}, %{"value" => 0.2}, %{"value" => 0.3}]}}
+    path = "$.data.values[1].value"
+
+    assert ExJsonPath.eval(map, path) == 0.2
+  end
+
+  test "eval a path with access to a filtered array" do
+    map = %{
+      "data" => [
+        %{
+          "values" => [
+            %{"value" => 0.1, "name" => "foo"},
+            %{"value" => 0.2, "name" => "bar"},
+            %{"value" => 0.3, "name" => "test"}
+          ]
+        }
+      ]
+    }
+
+    path = ~s{$.data[0].values[?(@.name == "test")].value}
+
+    assert ExJsonPath.eval(map, path) == [0.3]
+  end
+
+  test "eval a path with access to a filtered map" do
+    map = %{
+      "data" => [
+        %{
+          "values" => %{
+            "a" => %{"value" => 0.1, "name" => "foo"},
+            "b" => %{"value" => 0.2, "name" => "bar"},
+            "c" => %{"value" => 0.3, "name" => "test"}
+          }
+        }
+      ]
+    }
+
+    path = ~s{$.data[0].values[?(@.name == "test")].value}
+
+    assert ExJsonPath.eval(map, path) == [0.3]
+  end
+
+  test "eval a path with access to a filtered map and multiple results" do
+    map = %{
+      "data" => [
+        %{
+          "values" => %{
+            "a" => %{"value" => 0.1, "name" => "test"},
+            "b" => %{"value" => 0.2, "name" => "bar"},
+            "c" => %{"value" => 0.3, "name" => "test"}
+          }
+        }
+      ]
+    }
+
+    path = ~s{$.data[0].values[?(@.name == "test")].value}
+
+    assert ExJsonPath.eval(map, path) == [0.1, 0.3]
   end
 end
