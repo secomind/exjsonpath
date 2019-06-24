9d2665924d836e054acd0f16c78afe21b59de5d2
Davide Bettio <davide.bettio@ispirata.com>
2019-06-24 18:20:32 +0200

    Add excoveralls dep to mix.exs
    Add test coverage mix task

--- mix.exs
+++ mix.exs
@@ -7,22 +7,26 @@ defmodule ExJsonPath.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:excoveralls, "~> 0.11", only: :test}
     ]
   end
 end
