1f9ecfb389bea0622fe21728d7b364716acbbf61
Davide Bettio <davide.bettio@ispirata.com>
2020-03-24 16:19:08 +0100

    Update copyright to 2020

--- lib/ex_json_path.ex
+++ lib/ex_json_path.ex
@@ -1,7 +1,7 @@
 #
 # This file is part of ExJsonPath.
 #
-# Copyright 2019 Ispirata Srl
+# Copyright 2019,2020 Ispirata Srl
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
