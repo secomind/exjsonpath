cb1297796bc72310fea99c7634b66865a2640048
Davide Bettio <davide.bettio@ispirata.com>
2020-03-27 16:32:57 +0100

    recurse cannot return {:error, reason}, so just return result
    It doesn't make sense to return {:ok, result}, just return result.

--- lib/ex_json_path.ex
+++ lib/ex_json_path.ex
@@ -60,7 +60,7 @@ defmodule ExJsonPath do
   end
 
   def eval(input, compiled_path) when is_list(compiled_path) do
-    recurse(input, compiled_path)
+    {:ok, recurse(input, compiled_path)}
   end
 
   @doc """
@@ -94,7 +94,7 @@ defmodule ExJsonPath do
   end
 
   defp recurse(item, []) do
-    {:ok, [item]}
+    [item]
   end
 
   defp recurse(enumerable, [{:access, {op, path, value}} | t])
@@ -107,12 +107,12 @@ defmodule ExJsonPath do
             item -> item
           end
 
-        with {:ok, [value_at_path]} <- recurse(item, path),
+        with [value_at_path] <- recurse(item, path),
              true <- compare(op, value_at_path, value),
-             {:ok, [leaf_value]} <- recurse(item, t) do
+             [leaf_value] <- recurse(item, t) do
           [leaf_value | acc]
         else
-          {:ok, []} ->
+          [] ->
             acc
 
           false ->
@@ -120,25 +120,25 @@ defmodule ExJsonPath do
         end
       end)
 
-    {:ok, Enum.reverse(result)}
+    Enum.reverse(result)
   end
 
   defp recurse(map, [{:access, a} | t]) when is_map(map) do
     case Map.fetch(map, a) do
       {:ok, next_item} -> recurse(next_item, t)
-      :error -> {:ok, []}
+      :error -> []
     end
   end
 
   defp recurse(array, [{:access, a} | t]) when is_list(array) and is_integer(a) do
     case Enum.fetch(array, a) do
       {:ok, next_item} -> recurse(next_item, t)
-      :error -> {:ok, []}
+      :error -> []
     end
   end
 
   defp recurse(_any, [{:access, _a} | _t]) do
-    {:ok, []}
+    []
   end
 
   defp recurse(enumerable, [{:recurse, a} | t] = path)
@@ -151,22 +151,22 @@ defmodule ExJsonPath do
             item -> item
           end
 
-        {:ok, result} = recurse(item, path)
+        result = recurse(item, path)
         acc ++ result
       end)
 
     case safe_fetch(enumerable, a) do
       {:ok, item} ->
-        {:ok, rec} = recurse(item, t)
-        {:ok, rec ++ result}
+        rec = recurse(item, t)
+        rec ++ result
 
       :error ->
-        {:ok, result}
+        result
     end
   end
 
   defp recurse(_any, [{:recurse, _a} | _t]) do
-    {:ok, []}
+    []
   end
 
   defp safe_fetch(list, index) when is_list(list) and is_integer(index),
